f72df2a357dfabf60c1ee8078703d8da41982d88
eight1911 <15707729+Eight1911@users.noreply.github.com>
2017-11-02 01:43:03 -0400

    refactored, added extra check

--- tests.js
+++ tests.js
@@ -19,12 +19,14 @@ function test() {
     </p>
   </div>`
   let searcher = require("./replace.js")
-  let test = new searcher.HtmlSearch(html)
+  let test = new searcher.Search(html)
   let expected_text = `Take kindly the counsel of the years, gracefully surrendering the things of youth. Nurture strength of spirit to shield you in sudden misfortune. But do not distress yourself with dark imaginings. Many fears are born of fatigue and loneliness. Beyond a wholesome discipline, be gentle with yourself. You are a child of the universe, no less than the trees and the stars; you have a right to be _here_. And whether or not it is clear to you, no doubt the universe is unfolding as it "should."`
   let expected_html = `<div class="yourself"><p class="child">Take kindly the counsel of the years,<br>gracefully surrendering the things of youth.<br>Nurture strength of spirit to shield you in sudden misfortune.<br>But do not distress yourself with dark imaginings.<br>Many fears are born of fatigue and loneliness.<br>Beyond a wholesome discipline,<br>be gentle with yourself.<br></p><p class="child"><i>You</i>are a child of the universe,<br>no less than the trees and the stars;<br>you have a right to be _here_.<br>And whether or not it is clear to you,<br>no doubt the universe is unfolding as it "should."<br></p></div>`
   let altered_text = `Take kindly the counsel of the years, gracefully surrendering the things of weth. Nurture strength of spirit to shield we in sudden misfortune. But do not distress werself with dark imaginings. Many fears are born of fatigue and loneliness. Beyond a wholesome discipline, be gentle with werself. we are a child of the universe, no less than the trees and the stars; we have a right to be _here_. And whether or not it is clear to we, no doubt the universe is unfolding as it "should."`
   let altered_html = `<div class="yourself"><p class="child">Take kindly the counsel of the years,<br>gracefully surrendering the things of weth.<br>Nurture strength of spirit to shield we in sudden misfortune.<br>But do not distress werself with dark imaginings.<br>Many fears are born of fatigue and loneliness.<br>Beyond a wholesome discipline,<br>be gentle with werself.<br></p><p class="child"><i>we</i>are a child of the universe,<br>no less than the trees and the stars;<br>we have a right to be _here_.<br>And whether or not it is clear to we,<br>no doubt the universe is unfolding as it "should."<br></p></div>`
 
+
+  
 
   let expected_indx = [76, 120, 166, 290, 300, 371, 436]
   let matches = test.search('you', 'i')
@@ -45,6 +47,8 @@ function test() {
     throw "failure"
   if (test.html() !== altered_html)
     throw "failure"
+
+  console.log("all tests passed!")
 }
 
 test()
